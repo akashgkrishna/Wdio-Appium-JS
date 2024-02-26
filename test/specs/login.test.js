ebd6f83a8c4f3a64602b8d207268d9840ab62984
akashkrishna7 <akashkg@testvagrant.com>
2024-02-26 11:46:29 +0530

    Added test data management

--- test/specs/login.test.js
+++ test/specs/login.test.js
@@ -3,27 +3,33 @@ const Logger = require("../customlogger/Logger");
 const ProfileScreen = require("../screens/ProfileScreen");
 const Footer = require("../screens/common/Footer");
 const LoginFlow = require("../userflows/LoginFlow");
-require("dotenv").config();
+// require("dotenv").config();
+const dataLoader = require('../utils/dataLoader');
 
 describe('Login Flow', async function() {
     let loginFlow;
     let footer;
     let LOGGER;
     let profileScreen;
+    let credentials;
 
     before(async function() {
         loginFlow = new LoginFlow();
         footer = new Footer();
         LOGGER = new Logger();
         profileScreen = new ProfileScreen();
-        LOGGER.info("Starting Login Flow");
+        credentials = dataLoader.loadData('credentials.json');
     });
 
     it('should successfully perform the login flow', async function() {
+        const validCredentials = credentials.credentialsSets.validCredentials;
 
         // Act
+        await loginFlow.performLogin(validCredentials.username, validCredentials.password, validCredentials.otp);
+       
+        // Act
         // await loginFlow.performLogin("ulshopify@ultralesson.com", "12345", "0000");
-        await loginFlow.performLogin(process.env.TEST_USERNAME, process.env.TEST_PASSWORD, process.env.TEST_OTP);
+        // await loginFlow.performLogin(process.env.TEST_USERNAME, process.env.TEST_PASSWORD, process.env.TEST_OTP);
         await footer.clickOnProfileIcon();
 
         // Assert
---
+++ test/utils/dataLoader.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const dataDir = path.resolve(__dirname, '..', '..', 'resources');
+
+function loadData(fileName) {
+    try {
+        const filePath = path.join(dataDir, fileName);
+        return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    } catch (error) {
+        console.error(`Error loading data file ${fileName}:`, error);
+        return null;
+    }
+}
+
+module.exports = { loadData };
