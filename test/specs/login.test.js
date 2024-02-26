5101ddcf4ff67cf5651c4bc7d317c09ae8ae23a3
akashkrishna7 <akashkg@testvagrant.com>
2024-02-26 11:48:55 +0530

    Removed .env from dotenv

--- test/specs/login.test.js
+++ test/specs/login.test.js
@@ -3,7 +3,6 @@ const Logger = require("../customlogger/Logger");
 const ProfileScreen = require("../screens/ProfileScreen");
 const Footer = require("../screens/common/Footer");
 const LoginFlow = require("../userflows/LoginFlow");
-// require("dotenv").config();
 const dataLoader = require('../utils/dataLoader');
 
 describe('Login Flow', async function() {
@@ -22,14 +21,11 @@ describe('Login Flow', async function() {
     });
 
     it('should successfully perform the login flow', async function() {
+        // Arrange
         const validCredentials = credentials.credentialsSets.validCredentials;
 
         // Act
         await loginFlow.performLogin(validCredentials.username, validCredentials.password, validCredentials.otp);
-       
-        // Act
-        // await loginFlow.performLogin("ulshopify@ultralesson.com", "12345", "0000");
-        // await loginFlow.performLogin(process.env.TEST_USERNAME, process.env.TEST_PASSWORD, process.env.TEST_OTP);
         await footer.clickOnProfileIcon();
 
         // Assert
