b9045f5c5124b23bd7bfcdc996c92a17107d4660
akashkrishna7 <akashkg@testvagrant.com>
2024-02-26 13:20:14 +0530

    Updated base class for wait strategy and error handling

--- test/screens/base/Base.js
+++ test/screens/base/Base.js
@@ -1,26 +1,49 @@
 class BaseScreen {
+    async getElement(selector) {
+        if (typeof selector !== 'string' || !selector) {
+            throw new Error('Invalid selector provided');
+        }
+        return await $(selector);
+    }
+
     async click(selector) {
-        const element = await $(selector);
-        await element.waitForDisplayed();
+        const element = await this.getElement(selector);
+        await element.waitForDisplayed({ timeoutMsg: 'Element not displayed for click action' });
         await element.click();
     }
 
-    async clearText(selector) {
-        const element = await $(selector);
-        await element.waitForDisplayed();
-        await element.clearValue();
-    }
-
     async setValue(selector, value) {
-        const element = await $(selector);
+        const element = await this.getElement(selector);
         await element.setValue(value);
     }
 
+    async clearText(selector) {
+        const element = await this.getElement(selector);
+        await element.clearValue();
+    }
+
     async isDisplayed(selector) {
-        const element = await $(selector);
+        const element = await this.getElement(selector);
         return await element.isDisplayed();
     }
-    
+
+    async waitForDisplayed(selector) {
+        const element = await this.getElement(selector);
+        await element.waitForDisplayed({ timeoutMsg: 'Element not displayed' });
+    }
+
+    async getText(selector) {
+        const element = await this.getElement(selector);
+        return await element.getText();
+    }
+
+    async waitForText(selector, text) {
+        await browser.waitUntil(async () => {
+            const element = await this.getElement(selector);
+            const elementText = await element.getText();
+            return elementText.includes(text);
+        }, { timeoutMsg: 'Text not found' });
+    }
 }
 
 module.exports = BaseScreen;
--- test/userflows/LoginFlow.js
+++ test/userflows/LoginFlow.js
@@ -1,3 +1,4 @@
+const Logger = require("../customlogger/Logger");
 const BaseScreen = require("../screens/base/Base");
 const Footer = require("../screens/common/Footer");
 const LoginScreen = require("../screens/LoginScreen");
@@ -6,10 +7,13 @@ const ProfileScreen = require("../screens/ProfileScreen");
 const footer = new Footer();
 const profileScreen = new ProfileScreen();
 const loginScreen = new LoginScreen();
+const LOGGER = new Logger();
 
 class LoginFlow extends BaseScreen{
 
     async performLogin(username, password, otp){
+
+        LOGGER.info("Logging in to the application");
 
         await footer.clickOnProfileIcon();
         await profileScreen.clickOnLoginButton();
