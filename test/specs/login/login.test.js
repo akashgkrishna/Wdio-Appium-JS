66436ae6dd3c08b094cce0829743ae8d2e1e5395
akashkrishna7 <akashkg@testvagrant.com>
2024-03-06 12:15:57 +0530

    Changed to directly using json instead of json loader

--- test/specs/login/login.test.js
+++ test/specs/login/login.test.js
@@ -3,33 +3,31 @@ const Logger = require("../../customlogger/Logger");
 const ProfileScreen = require("../../screens/ProfileScreen");
 const Footer = require("../../screens/common/Footer");
 const LoginFlow = require("../../userflows/LoginFlow");
-const dataLoader = require('../../utils/dataLoader');
+const jsonCred = require("../../../resources/credentials.json")
 
 describe('Login Flow', async function() {
     let loginFlow;
     let footer;
     let LOGGER;
     let profileScreen;
-    let credentials;
 
     before(async function() {
         loginFlow = new LoginFlow();
         footer = new Footer();
         LOGGER = new Logger();
         profileScreen = new ProfileScreen();
-        credentials = dataLoader.loadData('credentials.json');
     });
 
     it('should successfully perform the login flow', async function() {
         // Arrange
-        const validCredentials = credentials.credentialsSets.validCredentials;
+        const validCredentials = jsonCred.credentialsSets.validCredentials;
 
         // Act
         await loginFlow.performLogin(validCredentials.username, validCredentials.password, validCredentials.otp);
         await footer.clickOnProfileIcon();
 
         // Assert
-        const isLoginSuccessful = await profileScreen.isLoginSuccessful();
+        const isLoginSuccessful = await profileScreen.isLoginedIn();
         assert.strictEqual(isLoginSuccessful, true, "Login was not successful");
 
         LOGGER.info("Login Flow completed successfully");
