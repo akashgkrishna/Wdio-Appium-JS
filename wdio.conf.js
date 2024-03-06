8b46073776debd7d0ef07048d12ebbc15e9164c2
akashkrishna7 <akashkg@testvagrant.com>
2024-03-06 12:15:12 +0530

    Increased timeout and reduced maxWorkers

--- wdio.conf.js
+++ wdio.conf.js
@@ -45,7 +45,7 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 10,
+    maxInstances: 1,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -145,7 +145,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout: 120000
     },
 
     //
